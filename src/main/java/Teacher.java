2e8b345cdf7b56d086b96d7ac51118939a13eb6c
HamedPoor84 <hamedpoor84@gmail.com>
2024-03-19 00:07:48 +0330

    creat Teacher class

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,35 @@
+public class Teacher extends Account
+{
+
+    public Teacher(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void Log_In(String name, String password) {
+
+    }
+
+    @Override
+    public void Sign_Up(String name, String password) {
+
+    }
+
+    public void Take_Courses()
+    {
+
+    }
+    public void Score_Students(){
+
+    }
+    public void Courses_List(){
+
+    }
+    public void Courses_Student(){
+
+    }
+    public void Show_Teachers_Score(){
+
+    }
+
+}
