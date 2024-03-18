5e954b87443a0d2cf593fed259fa7e80fc3e2389
HamedPoor84 <hamedpoor84@gmail.com>
2024-03-19 00:10:18 +0330

    filling functions in hagwarts class

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Hogwarts {
 
     // TODO: Define Attributes
@@ -5,15 +7,24 @@ public class Hogwarts {
 
 
     // TODO: Define Functionalities
-    public void viewAllTeachers() {
-        //TODO
+    public void viewAllTeachers(ArrayList<String> Teachers) {
+        for (int i = 0 ; i < Teachers.size() - 1 ; i++ )
+        {
+            System.out.println(Teachers.get(i));
+        }
     }
 
-    public void viewAllStudents() {
-        //TODO
+    public void viewAllStudents(ArrayList<String> Students) {
+        for (int i = 0 ; i < Students.size() - 1 ; i++ )
+        {
+            System.out.println(Students.get(i));
+        }
     }
 
-    public void viewAllCourses() {
-        //TODO
+    public void viewAllCourses(ArrayList<String> Courses) {
+        for (int i = 0 ; i < Courses.size() - 1 ; i++ )
+        {
+            System.out.println(Courses.get(i));
+        }
     }
 }
