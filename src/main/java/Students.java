25c4930f69ad436f600619fd83fddbbc9e00127c
HamedPoor84 <hamedpoor84@gmail.com>
2024-03-21 00:28:44 +0330

    creat student account

---
+++ src/main/java/Students.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Students extends Account {
+    private ArrayList<Course> Student_courses = new ArrayList<>() ;
+    private int score ;
+    public Students(String username, String password)
+    {
+        super(username, password);
+    }
+
+    public ArrayList<Course> getStudent_courses() {
+        return Student_courses;
+    }
+
+    public void setStudent_courses(ArrayList<Course> student_courses) {
+        Student_courses = student_courses;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void SetProfile ()
+    {
+
+    }
+    public void GetProfile ()
+    {
+
+    }
+
+}
