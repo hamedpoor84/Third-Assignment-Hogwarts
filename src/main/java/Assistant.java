42117cff2de9cdec3b98c03df077d8393e150306
HamedPoor84 <hamedpoor84@gmail.com>
2024-03-21 00:31:57 +0330

    delete some functions of assistant

--- src/main/java/Assistant.java
+++ src/main/java/Assistant.java
@@ -1,36 +1,21 @@
 import java.util.ArrayList;
 
 public class Assistant extends Account {
-    public ArrayList<Assistant> Assistant = new ArrayList<>() ;
 
     public Assistant(String username, String password) {
         super(username, password);
     }
 
-    @Override
-    public void Log_In(String username, String password) {
-    }
 
-    @Override
-    public void Sign_Up(String username, String password) {
-    }
-
-    public void Remove_Teacher (Teacher teacher)
+    public void Remove_Teacher (Hogwarts hagwarts , Teacher teacher)
     {
-        teacher.Remove_Teacher(teacher);
-    }
-    public void Courses_List ()
-    {
+        hagwarts.Remove_Teacher(teacher);
     }
     public void Courses_Students ()
     {
 
     }
     public void View_Profiles ()
-    {
-
-    }
-    public void Creat_course ()
     {
 
     }
